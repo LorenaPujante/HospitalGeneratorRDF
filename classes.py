edc715c1be659cda7a6d5f907e45055d56b253be
LorenaPujante <154461660+LorenaPujante@users.noreply.github.com>
2024-01-08 20:00:11 +0100

    Update classes.py

--- classes.py
+++ classes.py
@@ -118,7 +118,7 @@ class Patient:
         self.death = death
         self.stepLocations = {}     # IDs are saved
         self.episodes = []      # The objects are saved
-        self.seir = [None, None, None, None]     # To save in which step each state starts
+        self.seird = [None, None, None, None, None]     # To save in which step each state starts
 
 
 ##########
